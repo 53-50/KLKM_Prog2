92c773dbaa644298774175fb4e146fa1d8acc4d5
vanessastraka <vanessa.straka@stud.fh-campuswien.ac.at>
2025-02-23 21:28:43 +0100

    VS: 23.02.2025 - 6 movies added

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -33,6 +33,41 @@ public class Movie {
         List<Movie> movies = new ArrayList<>();
         // TODO add some dummy data here
 
+        Movie ironMan = new Movie("Iron Man", "After being held captive in an Afghan cave," +
+                "billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.");
+
+        Movie avengers = new Movie("The Avengers", "When an unexpected enemy emerges and " +
+                "threatens global safety and security, Nick Fury, director of the international" +
+                "peacekeeping agency known as S.H.I.E.L.D., finds himself in need of a team to pull" +
+                "the world back from the brink of disaster. Spanning the globe, a daring recruitment effort" +
+                "begins!");
+
+        Movie thor = new Movie("Thor", "Against his father Odin's will, The Mighty Thor " +
+                "- a powerful but arrogant warrior god - recklessly reignites an ancient war. Thor is " +
+                "cast down to Earth and forced to live among humans as punishment. Once here, Thor learns " +
+                "what it takes to be a true hero when the most dangerous villain of his world sends the " +
+                "darkest forces of Asgard to invade Earth.");
+
+        Movie schindler = new Movie("Schindler's List", "The true story of how businessman Oskar" +
+                " Schindler saved over a thousand Jewish lives from the Nazis while they worked as slaves" +
+                " in his factory during World War II.");
+
+        Movie godfather2 = new Movie("The Godfather Part II", "In the continuing saga of the Corleone " +
+                "crime family, a young Vito Corleone grows up in Sicily and in 1910s New York. In the 1950s, Michael " +
+                "Corleone attempts to expand the family business into Las Vegas, Hollywood and Cuba.");
+
+        Movie godfather1 = new Movie("The Godfather", "Spanning the years 1945 to 1955, " +
+                "a chronicle of the fictional Italian-American Corleone crime family. When organized crime" +
+                " family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son," +
+                " Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.");
+
+        movies.add(ironMan);
+        movies.add(avengers);
+        movies.add(thor);
+        movies.add(schindler);
+        movies.add(godfather1);
+        movies.add(godfather2);
+
         return movies;
     }
 }
