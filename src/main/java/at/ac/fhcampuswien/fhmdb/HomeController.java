a9b43c96d0bc0a375d8e6e437ab3d2edb89cc646
katrin <katrinholztrattner@gmail.com>
2025-02-25 22:37:08 +0100

    KH: 25.02.25 - added methode apply filter + event handler searchBtn & genreComboBox

--- src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/HomeController.java
@@ -15,6 +15,7 @@ import java.net.URL;
 import java.util.Comparator;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class HomeController implements Initializable {
     @FXML
@@ -27,10 +28,12 @@ public class HomeController implements Initializable {
     public JFXListView movieListView;
 
     @FXML
-    public JFXComboBox genreComboBox;
+    public JFXComboBox<Movie.Genre> genreComboBox;
 
     @FXML
     public JFXButton sortBtn;
+
+
 
     public List<Movie> allMovies = Movie.initializeMovies();
 
@@ -47,6 +50,7 @@ public class HomeController implements Initializable {
 
         // TODO add genre filter items with genreComboBox.getItems().addAll(...)
         genreComboBox.setPromptText("Filter by Genre");
+        genreComboBox.getItems().addAll(Movie.Genre.values()); //add Genres to ComboBox
 
         // TODO add event handlers to buttons and call the regarding methods
         // either set event handlers in the fxml file (onAction) or add them here
@@ -70,6 +74,33 @@ public class HomeController implements Initializable {
                 sortBtn.setText("Sort (asc)");
             }
         });
+
+        // Event handler search button and genreBox dropdown using apply filter methode
+        searchBtn.setOnAction(actionEvent -> applyFilter());
+        genreComboBox.setOnAction(actionEvent -> applyFilter());
+    }
+
+    //methode to apply a filter based on search request and/or chosen genre
+    private void applyFilter() {
+        String query = searchField.getText().toLowerCase(); //gets text from search request + transforms into lower cases (-> case insensitive)
+        Movie.Genre selectedGenre = genreComboBox.getValue(); //saves chosen genre
+
+        List<Movie> filteredMovies = allMovies.stream() //creates stream of movie list
+                //search request:
+                .filter(movie -> (query.isEmpty() || //search request is empty -> no search-filter
+
+                        //checks filmtitle and description -> filter by query
+                        movie.getTitle().toLowerCase().contains(query) ||
+                        //movie.getDescription().toLowerCase().contains(query)))
+                        (movie.getDescription() != null && movie.getDescription().toLowerCase().contains(query))))
+
+                //chosen genre:
+                .filter(movie -> (selectedGenre == null || //no selected genre -> no genre-filter
+                        movie.getGenre().contains(selectedGenre))) //checks if film contains genre
+
+                .collect(Collectors.toList()); //transforms filtered stream back into List
+
+        observableMovies.setAll(filteredMovies); //
     }
 
     public void sortMoviesAscending(){
